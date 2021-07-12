5053010f80142b5c6f2c7bee8a3d5bbf351a6ec0
facugarote <85139558+facugarote@users.noreply.github.com>
2021-07-12 14:43:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-const PORT = process.env.PORT || 3000;
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.listen(PORT, () => { 
-   console.log ("Servidor corriendo en puerto "+PORT);
+app.get('/', (req,res) =>{
+    res.sendFile(path.resolve(__dirname, './views/index.html'));  // Permite enviar un archivo HTML
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/', (req,res) =>{   
-    //res.send("Hola Mundo");  // Permite enviar texto o codigo HTML
-   res.sendFile(path.resolve(__dirname, './views/index.html'));   // Permite enviar un archivo HTML
-
+/*
+app.listen(3002, () => {
+    console.log("Servidor corriendo");
 });
+*/
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
