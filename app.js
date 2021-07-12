0e373d3cf09ceb01ac4cb5320b3df0faae3ab9c8
FacuGarrote <ceciefacugmail.com>
2021-07-12 14:08:38 -0300

    Carga de proyecto individual 2

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+   res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
+});
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/register.html'));  // Permite enviar un archivo HTML
+ });
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/login.html'));  // Permite enviar un archivo HTML
+ });
+
+app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './views')));
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo");
+});
