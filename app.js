7cf5857e2498e16b364766b1eaecd926ef08d9ac
facugarote <85139558+facugarote@users.noreply.github.com>
2021-07-12 14:23:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.get('/login', (req,res) =>{
 app.use(express.static(path.join(__dirname, './public')));
 app.use(express.static(path.join(__dirname, './views')));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
-});
+})
