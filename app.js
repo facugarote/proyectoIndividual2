137e692ce97f0cd96bb588df42d9528bec5ceeb3
facugarote <85139558+facugarote@users.noreply.github.com>
2021-07-12 14:44:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, './views/index.html'));  // Permite enviar un archivo HTML
+   res.sendFile(path.join(__dirname, './views/home.html'));  // Permite enviar un archivo HTML
 });
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/register.html'));  // Permite enviar un archivo HTML
+ });
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/login.html'));  // Permite enviar un archivo HTML
+ });
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './views')));
 
-/*
-app.listen(3002, () => {
-    console.log("Servidor corriendo");
-});
-*/
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
